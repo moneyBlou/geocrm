c7f9b26a5546df64ca4ff7f0606172e67e8fe00e
Ratmir <pochtaratmira@gmail.com>
2026-08-19 17:34:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.GEOCRM_CONFIG = {
-  googleClientId: "PASTE_GOOGLE_OAUTH_CLIENT_ID_HERE",
-  spreadsheetId: "PASTE_GOOGLE_SHEET_ID_HERE",
+  googleClientId: "1018915834164-bgdk6n3pubd7gbuoj1q64roqipl0ufq6.apps.googleusercontent.com",
+  spreadsheetId: "1vr8rQiilatrIs4GW1GXDjB3Ml2kzMs_PX4_QuDRoIsI",
   sheetName: "ОБЪЕКТЫ",
   yandexMapsApiKey: "PASTE_YANDEX_MAPS_API_KEY_HERE",
   mapCenter: [55.7558, 37.6173],
